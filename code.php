613e0fc721b1dc4da7e7a21c0591ba4b70fc30a4
Rito ndlovu <RitoNdlovu@outlook.com>
2022-03-29 10:06:16 +0200

    Update code.php

--- code.php
+++ code.php
@@ -35,6 +35,43 @@ if(isset($_POST['save_excel_data'])){
                     $count = "1";
             }
         }
+            
+                    $columnFilter->setFilterType(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column::AUTOFILTER_FILTERTYPE_FILTER
+            );
+
+            $columnFilter->createRule()->setRule(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column\Rule::AUTOFILTER_COLUMN_RULE_EQUAL,
+                'High'
+            );
+
+            $columnFilter->createRule()->setRule(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column\Rule::AUTOFILTER_COLUMN_RULE_EQUAL,
+                'Mediam'
+            );
+
+            $columnFilter->createRule()->setRule(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column\Rule::AUTOFILTER_COLUMN_RULE_EQUAL,
+                'Low'
+            );
+
+            $columnFilter->setFilterType(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column::AUTOFILTER_FILTERTYPE_CUSTOMFILTER
+            );
+
+            $columnFilter->createRule()->setRule(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column\Rule::AUTOFILTER_COLUMN_RULE_EQUAL,
+                'F*')->setRuleType(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column\Rule::AUTOFILTER_RULETYPE_CUSTOMFILTER,
+                'No'
+            );
+
+            $columnFilter->createRule()->setRule(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column\Rule::AUTOFILTER_COLUMN_RULE_EQUAL,
+                'F*')->setRuleType(
+                \PhpOffice\PhpSpreadsheet\Worksheet\AutoFilter\Column\Rule::AUTOFILTER_RULETYPE_CUSTOMFILTER,
+                'YES'
+            );
 
             if(isset($msg)){
                 $_SESSION['message']= 'succuddfully imported';
